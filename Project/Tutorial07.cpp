2be87c7b0fc96dbf72fcc3938f5e56adc56508a8
y1z <whyis2cool@gmail.com>
2019-09-16 23:07:16 -0500

    make cubes rotate,scale and both
    the left one only rototes, the middle one rotates and scales, the right
    one only scales.

--- Project/Tutorial07.cpp
+++ Project/Tutorial07.cpp
@@ -81,13 +81,13 @@ D3D_DRIVER_TYPE                     g_driverType = D3D_DRIVER_TYPE_NULL;
 D3D_FEATURE_LEVEL                   g_featureLevel = D3D_FEATURE_LEVEL_11_0;
 //ID3D11Device*                       g_pd3dDevice = NULL;
 //ID3D11DeviceContext*                g_pImmediateContext = NULL;
-IDXGISwapChain*                     g_pSwapChain = NULL;
+//IDXGISwapChain*                     g_pSwapChain = NULL;
 ID3D11RenderTargetView*             g_pRenderTargetView = NULL;
 ID3D11Texture2D*                    g_pDepthStencil = NULL;
 ID3D11DepthStencilView*             g_pDepthStencilView = NULL;
 ID3D11VertexShader*                 g_pVertexShader = NULL;
 ID3D11PixelShader*                  g_pPixelShader = NULL;
-ID3D11InputLayout*                  g_pVertexLayout = NULL;
+//ID3D11InputLayout*                  g_pVertexLayout = NULL;
 /*ID3D11Buffer*                       g_pVertexBuffer = NULL;
 ID3D11Buffer*                       g_pIndexBuffer = NULL;
 ID3D11Buffer*                       g_pCBNeverChanges = NULL;
@@ -288,7 +288,7 @@ HRESULT InitDevice()
     return hr;
 
   // Create a render target view
-  isSuccesful = my_swapChain.GetBuffer(my_backBuffer,0);
+  isSuccesful = my_swapChain.GetBuffer(my_backBuffer, 0);
   assert(("Error with swap-chain getting a buffer ", isSuccesful == true));
   //hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*) my_backBuffer.getTextureRef());
   //if (FAILED(hr))
@@ -666,15 +666,15 @@ void CleanupDevice()
   //if (g_pCBChangesEveryFrame) g_pCBChangesEveryFrame->Release();
   //if (g_pVertexBuffer) g_pVertexBuffer->Release();
   //if (g_pIndexBuffer) g_pIndexBuffer->Release();
-  if (g_pVertexLayout) g_pVertexLayout->Release();
+//  if (g_pVertexLayout) g_pVertexLayout->Release();
   if (g_pVertexShader) g_pVertexShader->Release();
   if (g_pPixelShader) g_pPixelShader->Release();
   if (g_pDepthStencil) g_pDepthStencil->Release();
   if (g_pDepthStencilView) g_pDepthStencilView->Release();
   if (g_pRenderTargetView) g_pRenderTargetView->Release();
-  if (g_pSwapChain) g_pSwapChain->Release();
-  // if (g_pImmediateContext) g_pImmediateContext->Release();
-   //  if (g_pd3dDevice) g_pd3dDevice->Release();
+  //  if (g_pSwapChain) g_pSwapChain->Release();
+    // if (g_pImmediateContext) g_pImmediateContext->Release();
+     //  if (g_pd3dDevice) g_pd3dDevice->Release();
 }
 
 
@@ -750,10 +750,24 @@ void Render()
   //                                           , D3D11_CLEAR_DEPTH, 1.0f
   //                                           , 0);
 
-  //
-  // Update variables that change once per frame
+  /* setup for manipulating 3d models */
+  XMVECTOR moveVector = {2,1,1,1};
+  XMVECTOR scaleVector = {1,1,std::fabs(std::cosf(t)),1};
+  XMVECTOR rotVector = {-t,0, t,1};
+  // CBChangesEveryFrame cb;
+  XMMATRIX tempMatrixMove = XMMatrixTranspose(XMMatrixTranslationFromVector(moveVector));
+  XMMATRIX tempMatrixScale = XMMatrixScalingFromVector(scaleVector);
+  XMMATRIX tempMatrixRotate = XMMatrixRotationRollPitchYawFromVector(rotVector);//XMMatrixRotationZ(-t);
+
+  XMVECTOR moveVector2 = {0,1,-1,1};
+  XMVECTOR scaleVector2 = {1,1,std::fabs(std::cosf(t)) * 2,1};
+  XMVECTOR rotVector2 = { 0,0, -t,1};
+
+  XMMATRIX tempMatrixMove2 = XMMatrixTranspose(XMMatrixTranslationFromVector(moveVector2));
+  XMMATRIX tempMatrixScale2 = XMMatrixScalingFromVector(scaleVector2);
+  XMMATRIX tempMatrixRotate2 = XMMatrixRotationRollPitchYawFromVector(rotVector2);//XMMatrixRotationZ(-t);
   CBChangesEveryFrame cb;
-  cb.mWorld = XMMatrixTranspose(g_World);
+  cb.mWorld = XMMatrixMultiply(XMMatrixMultiply(tempMatrixMove2, tempMatrixRotate2),tempMatrixScale2) ;
   cb.vMeshColor = g_vMeshColor;
   my_deviceContext.UpdateSubresource(&my_constChangesEveryFrame, &cb);
   //g_pImmediateContext->UpdateSubresource(my_constChangesEveryFrame.getBuffer(),
@@ -789,17 +803,20 @@ void Render()
   //g_pImmediateContext->PSSetSamplers(0, 1, my_sampler.getSamplerRef());
   //g_pImmediateContext->DrawIndexed(36, 0, 0);
 
-  XMVECTOR Move = {2,1,1,0};
-  // CBChangesEveryFrame cb;
-  cb.mWorld = XMMatrixTranspose(XMMatrixTranslationFromVector(Move));
+
+
+  cb.mWorld = XMMatrixMultiply(tempMatrixMove, tempMatrixScale);//XMMatrixTranspose(XMMatrixTranslationFromVector(moveVector));
+
   cb.vMeshColor = g_vMeshColor;
   my_deviceContext.UpdateSubresource(&my_constChangesEveryFrame, &cb);
 
   my_deviceContext.DrawIndexed(36, 0);
 
-  XMVECTOR Move2 = {-2,1,1,0};
+  XMVECTOR Move2 = {-3,1,1,0};
   // CBChangesEveryFrame cb;
-  cb.mWorld = XMMatrixTranspose(XMMatrixTranslationFromVector(Move2));
+  tempMatrixMove = XMMatrixTranspose(XMMatrixTranslationFromVector(Move2));
+
+  cb.mWorld = XMMatrixMultiply(tempMatrixMove, tempMatrixRotate);
   cb.vMeshColor = g_vMeshColor;
   my_deviceContext.UpdateSubresource(&my_constChangesEveryFrame, &cb);
 
