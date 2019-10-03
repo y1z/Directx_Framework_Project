ce50ed00463869a729432f4b6127f433ccd62b31
y1z <whyis2cool@gmail.com>
2019-10-03 01:18:53 -0500

    make archive before installing direct DirectXTK.

--- Project/Tutorial07.cpp
+++ Project/Tutorial07.cpp
@@ -5,7 +5,7 @@
 //
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //--------------------------------------------------------------------------------------
-
+/*! copy before update */
 #include "../include/utiliy/Grafics_libs.h"
 
 /******************************************************/
