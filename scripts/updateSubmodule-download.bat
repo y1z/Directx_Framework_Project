33779480ab7d007c1eb182eda3dd30e11aa83660
y1z <whyis2cool@gmail.com>
2019-12-06 01:15:01 -0600

    fix 'updateSubmodule-download.bat' script

--- scripts/updateSubmodule-download.bat
+++ scripts/updateSubmodule-download.bat
@@ -4,24 +4,12 @@ echo 'adding the submodules to the project '
 REM this variable give the path to the current batch file
 cd %~dp0
 
-REM here are the names of the directory for the shaders.
-
-SET glDir=GlShaders
-SET dxDir=DxShaders
-
-REM creating the directorys for the project
-
+REM going 1 directory to later download the repos for the shaders 
 cd .. 
-REM access the variable
-mkdir %glDir%
-cd %glDir%
+
 echo 'downloadin glsl repo'
-REM dowloadin the glsl shaders
 git clone https://github.com/y1z/GlShaders
 
-cd ..
-mkdir %dxDir%
-cd %dxDir%
 echo 'downloadin hlsl repo'
 git clone https://github.com/y1z/DxShaders
 
